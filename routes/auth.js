6300e03fb143f614d5fbf2e6a1d256c467f6ee27
Mehedi-Developer <mehedihasan30313@gmail.com>
2021-10-30 10:35:10 +0600

    Updated server code

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,11 @@ router.post("/register", async (req, res) => {
     //generate new password
     // const existUser = await User.findOne({ email: req.body.email });
     // existUser && res.status(400).json("This email user is already registered");
-
+    // console.log("User Body   ================  ", req?.body);
     const salt = await bcrypt.genSalt(10);
+    // console.log({salt});
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // console.log({hashedPassword});
     //create new user
     const newUser = new User({
       profilePicture: req.body.profilePicture || "",
@@ -23,6 +25,7 @@ router.post("/register", async (req, res) => {
     //save user and respond
     const user = await newUser.save();
     res.status(200).json({...user?._doc, userId: user?._id});
+
   } catch (err) {
     res.status(500).json({
       statusCode: 500,
@@ -37,9 +40,9 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).json({massage: "User not found", statusCode: 404});
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
     !validPassword && res.status(400).json({message: "Wrong password", statusCode: 404})
-    console.log({user: user?._doc});
+    // console.log({user: user?._doc});
     res.status(200).json({...user?._doc, userId: user?._id})
   } catch (err) {
     res.status(500).json(err)
--- routes/conversations.js
+++ routes/conversations.js
@@ -15,21 +15,21 @@ const addConversation = async(senderId, receiverId, res) => {
     res.status(500).json(err);
   }
 }
-router.post("/", async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
-  });
 
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.post("/", async (req, res) => {
+//   const newConversation = new Conversation({
+//     members: [req.body.senderId, req.body.receiverId],
+//   });
+
+//   try {
+//     const savedConversation = await newConversation.save();
+//     res.status(200).json(savedConversation);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // get conversation of a user
-
 router.get("/:userId", async (req, res) => {
   try {
     const conversation = await Conversation.find({
@@ -42,7 +42,6 @@ router.get("/:userId", async (req, res) => {
 });
 
 // get conversation includes two userId
-
 router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
   try {
     const conversation = await Conversation.findOne({
